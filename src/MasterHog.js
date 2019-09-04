d409763572a1ffa277fa852098e571bf92b26801
github username <github email address>
2019-09-04 19:48:28 +0100

    Done.

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,52 +1,69 @@
-import React, { Component } from 'react'
-import Master from './assets/master-hog.png'
-import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import React, { Component } from "react";
+import Master from "./assets/master-hog.png";
+import BabyHog from "./BabyHog";
+import offspring from "./db.js";
 
 export default class MasterHog extends Component {
+  state = {
+    eyeColor: ""
+  };
 
-  constructor() {
-    super()
-    this.state = {
-      eyeColor: "blue",
-    }
-  }
-
-
-  changeEyeColor = (e) => {
+  changeEyeColor = e => {
     this.setState({
       eyeColor: e.target.value
-    })
-  }
-
+    });
+  };
 
   render() {
+    const { eyeColor } = this.state;
+
     return (
       <div>
-
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="blue"
+          onChange={this.changeEyeColor}
+        />
         Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="sun"
+          onChange={this.changeEyeColor}
+        />
         Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
+        <input
+          type="radio"
+          name="eyeColor"
+          value="glowing"
+          onChange={this.changeEyeColor}
+        />
         Glowing<br></br>
-      
-        <h2>Name: Master Blaster</h2>
+        <h2>Name: Mater Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          <BabyHog
+            eyeColor={eyeColor}
+            name={offspring[0]["name"]}
+            hobby={offspring[0]["hobby"]}
+          />
+          <BabyHog
+            eyeColor={eyeColor}
+            name={offspring[1]["name"]}
+            hobby={offspring[1]["hobby"]}
+          />
+          <BabyHog
+            eyeColor={eyeColor}
+            name={offspring[2]["name"]}
+            hobby={offspring[2]["hobby"]}
+          />
         </ul>
-
       </div>
-    )
+    );
   }
-
 }
